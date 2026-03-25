d6a15d61a2a71cbafa0f7b30a35c5229b7cd58fd
Sahan Rajapakshe <sahansamadhi143@gmail.com>
2026-03-25 16:15:45 +0530

    process diabled

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,6 @@ const App = () => {
                  <>
                    <Services />
                    <Why />
-                   <Process />
                    <Contact />
                    <Footer />
                  </>
